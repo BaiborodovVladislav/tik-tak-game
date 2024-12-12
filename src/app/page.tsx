6d2821135a3f5f59f67318cb3aed871adabf88fb
Vladislav <baiborodov333@mail.ru>
2024-12-12 23:53:56 +0500

    setings-checkpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-import { Button } from '@/shared/ui/button'
+import { prisma } from "@/shared/lib/db";
+import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
+export default async function Home() {
+  const games = await prisma.game.findMany();
+  console.log(games);
 
-export default function Home() {
   return (
     <div className="">
-      <Button size="lg" variant={'destructive'}>Hello</Button>
+      <Button size="lg" variant={"destructive"}>
+        Hello
+      </Button>
+      {games.map((game) => {
+        return (
+          <Card key={game.id}>
+            <CardTitle>{game.name}</CardTitle>
+          </Card>
+        );
+      })}
     </div>
   );
 }
